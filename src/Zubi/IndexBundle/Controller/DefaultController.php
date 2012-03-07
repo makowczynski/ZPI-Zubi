12cfc71d6b5c39df7c695844195e9ec7690e0229
Pawel Junak <pawel.junak@gmail.com>
2012-03-07 22:44:58 +0100

    koniec fixow na dzis.

--- src/Zubi/IndexBundle/Controller/DefaultController.php
+++ src/Zubi/IndexBundle/Controller/DefaultController.php
@@ -8,9 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('ZubiIndexBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('ZubiIndexBundle:Default:index.html.twig');
     }
     
 }
