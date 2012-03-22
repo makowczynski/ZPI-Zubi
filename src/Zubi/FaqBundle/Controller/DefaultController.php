d34812db2a0a79268d1cc45a6b8e4a9f3627deae
mielony <mielniczuk.mariusz@gmail.com>
2012-03-23 00:05:00 +0100

    Poprawa dodawania FAQ + validacja

--- src/Zubi/FaqBundle/Controller/DefaultController.php
+++ src/Zubi/FaqBundle/Controller/DefaultController.php
@@ -4,40 +4,36 @@ namespace Zubi\FaqBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Zubi\FaqBundle\Entity\Faq;
+use Zubi\FaqBundle\Form\Faq\FaqForm;
 use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     
     public function indexAction(Request $request)
     {
+        
      $newFaq = new Faq();
-     $form = $this->createFormBuilder($newFaq)     
-             ->add('Tresc', 'text')
-             ->add('odpowiedz', 'text')
-             ->add('id_statusu', 'text')
-             ->getForm();
-     
-      if($request->getMethod() == 'POST') {
-          
-            $form->bindRequest($request);
-         
-            if($form->isValid()) {
-                 $em = $this->getDoctrine()->getEntityManager();
-                 $em->persist($newFaq );
-                 $em->flush();
-               // $newFaq->$form->getData();
-            }
-      } 
-     
+     $form = $this->createForm(new FaqForm(), $newFaq);      
+     if($request->getMethod() == 'POST') {          
+        $form->bindRequest($request);         
+        $validator = $this->get('validator');
+            $errors = $validator->validate($newFaq);
+        if (count($errors) < 1) {
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($newFaq );
+                $em->flush();
+                //po poprawnym dodaniu danych z formularza, chcemy mieć go pustego.
+                $newFaq = new Faq();
+                $form = $this->createForm(new FaqForm(), $newFaq);                 
+        }                     
+     }       
+     //pobieramy z bazy wszystkie faq
      $faqs = $this->getDoctrine()
         ->getRepository('ZubiFaqBundle:Faq')
         ->findAll();
-     //  $faq = $faqs[0];
-      // foreach ($faqs as $faq) {
-      //     
-      // }
-      // $res="dupa";
         return $this->render('ZubiFaqBundle:Default:index.html.twig', 
                 array('faqs'=>$faqs, 
                     'form'=>$form->createView()));
