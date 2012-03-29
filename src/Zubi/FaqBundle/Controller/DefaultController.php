0f07f4c9182c6099f3f54b98d72435bc804a2cd6
mielony <mielniczuk.mariusz@gmail.com>
2012-03-29 22:13:29 +0200

    Dodane usuwanie FAQ.

--- src/Zubi/FaqBundle/Controller/DefaultController.php
+++ src/Zubi/FaqBundle/Controller/DefaultController.php
@@ -12,11 +12,35 @@ use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+    //kontroler odpowiadajcy za usuwanie FAQ
+    public function deleteAction($id) {
+        $em = $this->getDoctrine()->getEntityManager();                               
+        //pobieramy z bazy FAQ do skasowania.
+        $delFaq = $this->getDoctrine()
+                ->getRepository('ZubiFaqBundle:Faq')
+                ->findOneById($id);
+         
+        if ($delFaq) {
+            // kasujemy FAQ 
+            $em->remove($delFaq);
+            $em->flush();
+            // przekierowanie na index z FAQ
+            $this->get('session')->setFlash('notice', 'Skasowałeś FAQ pt: "'.$delFaq->getTresc().'"');
+            return $this->redirect($this->generateUrl('ZubiFaqBundle_homepage'));
+                        
+            // TODO: Może jakieś pytanie czy na 100% usunąć? Na razie nie ma
+        }
+        else {
+            $this->get('session')->setFlash('errorMsg', 'Nie ma czego kasowac, nie ma FAQ o id: '.$id.'!');
+            return $this->redirect($this->generateUrl('ZubiFaqBundle_homepage'));
+        }          
+    }
+    
     
     public function indexAction(Request $request)
     {
-        
      $newFaq = new Faq();
+     $em = $this->getDoctrine()->getEntityManager();    
      $form = $this->createForm(new FaqForm(), $newFaq);      
      //jeśli strona wyswietla się po przesłaniu formularza w POST
      //trzeba spróbować dodać dane do bazy danych
@@ -26,28 +50,32 @@ class DefaultController extends Controller
             $errors = $validator->validate($newFaq);
         //jeśli przesyłane dane są poprawne
         //dodajemy je do bazy oraz czyścimy formularz.
-        if (count($errors) < 1) {
-                $em = $this->getDoctrine()->getEntityManager();                               
-               // $sw = new Status_widocznosci();
-                echo "<h1>".$newFaq->getIdStatusu()."</h1>";
-                 $sw = $this->getDoctrine()
+        if (count($errors) < 1) {                                                                          
+                $sw = $this->getDoctrine()
                         ->getRepository('ZubiFaqBundle:Status_widocznosci')
                         ->findOneByNazwa($newFaq->getIdStatusu());
                 $newFaq->setStatusWidocznosci($sw);
                 $em->persist($newFaq );
                 $em->flush();
-                //po poprawnym dodaniu danych z formularza, chcemy mieć go pustego.
-                //$newFaq = new Faq();
-                //$form = $this->createForm(new FaqForm(), $newFaq);                 
+                 $this->get('session')->setFlash('notice', 'Poprawnie dodałeś nowe FAQ.');
+                //po poprawnym dodaniu danych z formularza, chcemy mieć go pustego.                
+                $newFaq = new Faq();
+                $form = $this->createForm(new FaqForm(), $newFaq);                 
         }                     
      }       
-     //pobieramy z bazy wszystkie faq
-     $faqs = $this->getDoctrine()
-        ->getRepository('ZubiFaqBundle:Faq')
-        ->findAll();
+     //pobieramy z bazy wszystkie faq     
+     $faqs = $em->getRepository('ZubiFaqBundle:Faq')->findAll();     
+     // dla każdego faq tworzę link do jego usunięcia.
+     $a = 0;
+     $delLinks[0]="";
+     foreach ($faqs as $faq) {
+         $delLinks[$a] = $this->generateUrl('ZubiFaqBundle_delete', array ('id' => $faq->getId() ));
+         $a++;                 
+     }
         return $this->render('ZubiFaqBundle:Default:index.html.twig', 
-                array('faqs'=>$faqs, 
-                      'form'=>$form->createView()));
+                array('faqs' => $faqs, 
+                      'delLinks' => $delLinks,
+                      'form' => $form->createView()));
     }
     
 }
