6b9b4c5e5fbca85e832024cd4f161df0c9162b9a
mielony <mielniczuk.mariusz@gmail.com>
2012-03-30 01:30:21 +0200

    po awarii - jest OK

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Zubi\DeviceBundle\ZubiDeviceBundle(),
             new Zubi\FaqBundle\ZubiFaqBundle(),
             new Zubi\UserBundle\ZubiUserBundle(),
+            new Zubi\ArticleBundle\ZubiArticleBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Zubi/ArticleBundle/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Zubi\ArticleBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+    
+    public function indexAction()
+    {
+        return $this->render('ZubiArticleBundle:Default:index.html.twig');
+    }
+}
